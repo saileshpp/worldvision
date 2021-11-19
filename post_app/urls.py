d4c50b63f0e8daec5b67cffcc1bca4d95991c8b1
shaileshpujari <shaileshpuzari>
2021-11-19 09:35:17 +0545

    added a lot of features

--- post_app/urls.py
+++ post_app/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('signout', views.signout, name='signout'),
     path('addpost', views.addpost, name='addpost'),
     path('viewpost/<int:id>', views.viewpost, name='viewpost'),
+    path('delete/<int:id>', views.delete, name='delete'),
     path('myaccount',views.myaccount, name='myaccount'),
     path('changepassword',views.changepassword, name='changepassword'),
+    path('mypost',views.mypost, name='mypost'),
 ]
--- post_app/views.py
+++ post_app/views.py
@@ -13,7 +13,8 @@ from post_app.models import Post
 def index(request):
     news = Post.objects.all()
     b_news = random.sample(list(news),4)
-    return render(request, 'index.html', {'date': date.today(),'news':news,'b_news':b_news,'aside': news[:4]})
+    carousel_news = random.sample(list(news),3)
+    return render(request, 'index.html', {'date': date.today(),'news':news,'b_news':b_news,'aside': news[:4],'c_news':carousel_news})
 
 def signin(request):
     if request.method == 'GET':
@@ -25,7 +26,12 @@ def signin(request):
 
         if user is not None:
             login(request, user)
-            return redirect('index')
+            url = request.GET.get('next')
+           
+            if url is not None:
+                return redirect(url)
+            else: 
+                return redirect('index')
         
         else:
             messages.error(request,'Invalid Username or Password')
@@ -35,7 +41,7 @@ def signout(request):
     logout(request)
     return redirect('index')
 
-@login_required(login_url='signin', redirect_field_name='addpost.html')
+@login_required(login_url='signin')
 def addpost(request):
     if request.method == 'GET':
         return render(request, 'addpost.html',{'date': date.today()})
@@ -43,10 +49,11 @@ def addpost(request):
         title = request.POST['title']
         body = request.POST['body']
         cat = request.POST['cat']
-        file = request.POST['file']
+        file = request.FILES['image']
 
         item = Post.objects.create(post_title=title,post_body=body,category=cat,img=file,author = request.user)
         item.save()
+        messages.success(request,'News added succesfully')
         return redirect('index')
 
 def viewpost(request, id):
@@ -56,7 +63,6 @@ def viewpost(request, id):
     
 @login_required(login_url='signin')
 def myaccount(request):
-    print(request.user.password)
     return render(request, 'myaccount.html')
 
 @login_required(login_url='signin')
@@ -65,8 +71,6 @@ def changepassword(requset):
     new_password = requset.POST['new_password']
     password = requset.user.password
     check = check_password(current_password, password)
-
-    print(check)
     
     if check:
         u = User.objects.get(username=requset.user)
@@ -75,3 +79,14 @@ def changepassword(requset):
         return redirect('myaccount')
     else:
         return HttpResponse('FAKE PASSWORD')
+
+@login_required(login_url='signin')
+def mypost(request):
+    news = Post.objects.filter(author = request.user)
+    return render(request, 'mypost.html', {'news': news})
+
+@login_required(login_url='signin')
+def delete(request, id):
+    news = Post.objects.get(id=id)
+    news.delete()
+    return redirect('mypost')
