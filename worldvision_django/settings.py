aae73dde1671140562d2d7ed933668eb0adce58c
shaileshpujari <shaileshpuzari>
2021-12-01 12:22:07 +0545

    asd

--- worldvision_django/settings.py
+++ worldvision_django/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-import dj_database_url
-from decouple import config
 
 from django.db.models.manager import EmptyManager
 
