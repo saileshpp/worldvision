810a79da9ed502847dd224a435674a5e1cd61114
shaileshpujari <shaileshpuzari>
2021-12-01 12:10:14 +0545

    asdj

--- static/js/navbaractive.js
+++
@@ -1,8 +0,0 @@
-const currentLocation = location.href;
-const menuItem = document.querySelectorAll(" ")
-const menuLength = menuItem.length
-
-for (let i = 0; i < menuLength; i++){
-    if(menuItem[i].href === currentLocation)
-        menuItem[i].className = "active"
-}
